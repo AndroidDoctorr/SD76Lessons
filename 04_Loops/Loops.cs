bbf48011ac7d552f1a862ed3576e4703dadb0a82
Andrew Torr <gbartthenerd@gmail.com>
2021-01-27 09:53:52 -0500

    loops and conditionals challenge

--- 04_Loops/Loops.cs
+++ 04_Loops/Loops.cs
@@ -76,5 +76,36 @@ namespace _04_Loops
                 Console.WriteLine(i);
             }
         }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            string sillyWord = "Supercalifragilisticexpialidocious";
+            int count = 0;
+            foreach (char letter in sillyWord)
+            {
+                if (letter == 'i' || letter == 'l')
+                {
+                    Console.WriteLine(letter);
+                }
+                else if (letter == 'l') {
+                    Console.WriteLine("L");
+                }
+                else
+                {
+                    Console.WriteLine("Not 'i', said the Console");
+                }
+
+                count++;
+            }
+
+            Console.WriteLine(sillyWord.Length);
+
+            for (int i=0; i< sillyWord.Length; i++)
+            {
+                char letter = sillyWord[i];
+                // "" same as foreach from here
+            }
+        }
     }
 }
