3d59d0bc550ab26060de0a2dfe578f65eb5127bb
seth-d-mclaughlin <seth-d-mclaughlin@gmail.com>
2021-02-10 13:28:05 -0500

    Added practice examples

---
+++ ArtGallery/ArtTypes/Relief.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtGallery.ArtTypes
+{
+    public class Relief : Sculpture
+    {
+        public double Height { get; }
+        public double Width { get; }
+
+        public Relief(double height, double width, string title, string artistName, DateTime dateCreated, double weight) : base(artistName, title, dateCreated, weight)
+        {
+            Height = height;
+            Width = width;
+        }
+    }
+}
